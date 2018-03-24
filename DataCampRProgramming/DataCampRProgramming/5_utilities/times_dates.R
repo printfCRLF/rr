a7762c0db51ed045b939dddec45567e1e0bbfce3
Bowen <bowen.sui@hotmail.com>
2018-03-24 17:48:57 +0000

    new course. introduction of R to finance

--- DataCampRProgramming/DataCampRProgramming/5_utilities/times_dates.R
+++ DataCampRProgramming/DataCampRProgramming/5_utilities/times_dates.R
@@ -41,7 +41,23 @@ create_format_times <- function() {
     print(result)
 }
 
+calculation_with_dates <- function() {    
+    day1 <- as.Date("2018-03-06")
+    day2 <- as.Date("2018-03-08")
+    day3 <- as.Date("2018-03-13")
+    day4 <- as.Date("2018-03-19")
+    day5 <- as.Date("2018-03-24")
+    print(day5 - day1)
+
+    pizza <- c(day1, day2, day3, day4, day5)
+    day_diff = diff(pizza)
+    mean <- mean(day_diff)
+    print(mean)
+
+}
+
 #right_here_right_now()
 #create_format_dates()
-create_format_times()
+#create_format_times()
+calculation_with_dates()
 
---
+++ DataCampRProgramming/IntroRFinance/1_basics/financial_returns.R
@@ -0,0 +1,24 @@
+financial_returns1 <- function() {
+    # Variables for starting_cash and 5% return during January
+    starting_cash <- 200
+    jan_ret <- 5
+    jan_mult <- 1 + (jan_ret / 100)
+
+    # How much money do you have at the end of January?
+    post_jan_cash <- starting_cash * jan_mult
+
+    # Print post_jan_cash
+    print(post_jan_cash)
+
+    # January 10% return multiplier
+    jan_ret_10 <- 10
+    jan_mult_10 <- 1 + 10 / 100
+
+    # How much money do you have at the end of January now?
+    post_jan_cash_10 <- starting_cash * jan_mult_10
+
+    # Print post_jan_cash_10
+    print(post_jan_cash_10)
+}
+
+financial_returns1()
