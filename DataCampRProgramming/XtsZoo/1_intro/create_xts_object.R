fdf75d8a39f26b15cb90ad1c48734bf1d8c52018
Bowen <bowen.sui@hotmail.com>
2018-03-27 10:07:33 +0100

    importing and exporting xts data

--- DataCampRProgramming/XtsZoo/1_intro/create_xts_object.R
+++ DataCampRProgramming/XtsZoo/1_intro/create_xts_object.R
@@ -1,3 +1,5 @@
+library(xts)
+
 # xts = matrix + times (as index)
 # xts() constructor 
 # x - for data and order.by for the index 
@@ -14,9 +16,29 @@ first_xts_object <- function() {
     # born = bday creats key-value pairs to hold custom metadata 
     hayek <- xts(x = data, order.by = dates, born = bday)
 
+    print("return core data")
     hayek_core <- coredata(hayek)
     print(hayek_core)
+
+    print("class(hayek_core)")
+    print(class(hayek_core))
+
+    print("index(hayek)")
+    hayek_index <- index(hayek)
+    print(hayek_index)
 }
 
-first_xts_object()
+time_based_indices <- function() {
+    dates <- as.Date("2016-01-01") + 0:4
+    ts_a <- xts(x = c(1, 2, 3, 4, 5), order.by = dates)
+    ts_b <- xts(x = c(1, 2, 3, 4, 5), order.by = as.POSIXct(dates))
+    result <- ts_a[index(ts_b)]
+    print(result)
+    result <- ts_b[index(ts_a)]
+    print(result)
+}
+
+
+#first_xts_object()
+time_based_indices()
 
